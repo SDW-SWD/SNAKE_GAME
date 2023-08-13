c22c0bb07a19f5e54a974697ee49233223dad042
SDW-SWD <54956598+SDW-SWD@users.noreply.github.com>
2023-08-13 17:18:42 +0530

    Update GamePanel.java

--- project1/src/project1/GamePanel.java
+++ project1/src/project1/GamePanel.java
@@ -128,7 +128,7 @@ public class GamePanel extends JPanel implements ActionListener {
 		}
 		//check if head touches left border
 		if(x[0] < 0) {
-			running = false;
+			running = false;//looping method 
 		}
 		//check if head touches right border
 		if(x[0] > SCREEN_WIDTH) {
